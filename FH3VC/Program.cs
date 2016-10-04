16cf52e9e95363406ad74c2198610753b3cd724b
Steven <steven.wobser@gmail.com>
2016-10-04 20:20:28 +0200

    Prevent multiple instances
    *increased version to 1.0.0.1

--- FH3VC/Program.cs
+++ FH3VC/Program.cs
@@ -1,18 +1,20 @@
 ﻿using ForzaVolumeControl.App;
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ForzaVolumeControl {
-
     internal class Program {
+        private const string AppGuid = "9F334466-5ACA-43E6-8761-A6CC7D21A5D4";
 
         [STAThread]
         private static void Main(string[] args) {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            using (var context = new Context()) {
-                Application.Run(context);
+            using (var mutex = new Mutex(false, $"Global\\{AppGuid}")) {
+                if (!mutex.WaitOne(0, false)) return;
+                Application.Run(new Context());
             }
         }
     }
